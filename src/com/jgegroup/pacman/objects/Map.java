0c7c1aeafbfed79a6e5e7e575bcfb4372277edd9
Orideus <Orideus@users.noreply.github.com>
2023-01-10 18:03:17 -0800

    Created the basic code for player, basically content and a bit more from the meeting

---
+++ src/com/jgegroup/pacman/objects/Map.java
@@ -0,0 +1,14 @@
+package src.com.jgegroup.pacman.objects;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class Map {
+    HashMap<Point, Integer> board;
+    private Map(){
+        //TODO:Initialize the board;
+    }
+
+    public static Point[] getMap(){return null;};
+    public static void updateMap(){};
+}
