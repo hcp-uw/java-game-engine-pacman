5e2c69163158dd14c73de3be1f1b9b557c452d0e
noah <ncrouch1@uw.edu>
2023-01-14 17:51:41 -0800

    Added Position class, changed Player to abstact

---
+++ src/com/jgegroup/pacman/objects/Position.java
@@ -0,0 +1,44 @@
+package jgegroup.pacman.objects;
+
+public class Position {
+    private double x;
+    private double y;
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void translate(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public boolean equals(Position other) {
+        if (this.x == other.getX() && this.y == other.getY()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void moveTo(double x, double y) {
+        this.setX(x);
+        this.setY(y);
+    }
+}
