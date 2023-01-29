10a53c9bdecbf7aeaa5da1c3415965ae6bcfa182
trillyh <115189687+trillyh@users.noreply.github.com>
2023-01-28 22:36:02 -0800

    Update Main.java

--- src/com/jgegroup/pacman/Main.java
+++ src/com/jgegroup/pacman/Main.java
@@ -1,9 +1,22 @@
 package com.jgegroup.pacman;
+import javax.swing.*;
 
 public class Main {
     //Code starts here
     public static void main(String[] args) {
-        // Todo: implement a method or procedure to get screen resolution then split into tiles using data.
+        JFrame frame = new JFrame();
 
+        //Visibility
+        frame.setTitle("Pac Chilling Game");
+        frame.setVisible(true);
+
+        //Others Stuff
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // if map out of the way, add this frame.setResizeable(false);
+
+        // Add Gamepanel to Jpanel
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.pack();  // make Jframe window size same as largest Jpanel size. So frame'size = gamePanel's size.
     }
 }
