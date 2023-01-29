d0f2b75fef9464838657b42050a1d228d5aa6d80
trillyh <115189687+trillyh@users.noreply.github.com>
2023-01-28 21:33:08 -0800

    add comment
    add comment

--- src/com/jgegroup/pacman/Main.java
+++ src/com/jgegroup/pacman/Main.java
@@ -2,6 +2,7 @@ package com.jgegroup.pacman;
 
 public class Main {
     //Code starts here
+    // hello
     public static void main(String[] args) {
         // Todo: implement a method or procedure to get screen resolution then split into tiles using data.
 
