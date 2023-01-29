7ce8588d51b49b808b714c387abdd95ee3d0f762
trillyh <115189687+trillyh@users.noreply.github.com>
2023-01-28 22:38:05 -0800

    Add files via upload

---
+++ src/com/jgegroup/pacman/GamePanel.java
@@ -0,0 +1,21 @@
+package com.jgegroup.pacman;
+import com.jgegroup.pacman.objects.TileManager;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+   public final int NUMBER_OF_TILE_VERTICAL = 36;
+   public final int NUMBER_OF_TILE_HORIZONTAL = 28;
+   public final int TileSize = 32;
+
+   public final int RESOLUTION_VERTICAL = NUMBER_OF_TILE_VERTICAL*TileSize; // NUMBER_OF_TILE_VERTICAL * TileSize = 1152
+   public final int RESOLUTION_HORIZONTAL = NUMBER_OF_TILE_HORIZONTAL*TileSize;// =NUMBER_OF_TILE_HORIZONTAL * Tilesize = 896
+
+    public GamePanel () { // Constructor GamePanel
+        this.setPreferredSize(new Dimension(RESOLUTION_HORIZONTAL, RESOLUTION_VERTICAL));
+        this.setDoubleBuffered(true); // improve performance
+        this.setBackground(Color.pink); // pink is cool!
+    }
+}
