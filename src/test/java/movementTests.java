9b1cc05859b421776c86520ae59a40b36721c71e
ncrouch1 <102344384+ncrouch1@users.noreply.github.com>
2023-02-21 17:40:55 -0800

    New thing so tung can pull it

--- src/test/java/movementTests.java
+++ src/test/java/movementTests.java
@@ -1,11 +1,31 @@
-package test.java;
-
+import com.jgegroup.pacman.objects.Enums.*;
+import com.jgegroup.pacman.objects.Ghost;
+import com.jgegroup.pacman.objects.Position;
+import com.jgegroup.pacman.objects.Tile;
+import com.jgegroup.pacman.objects.immovable.Path;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class movementTests {
+    public static final String testImageUrl =
+            "https://developer.mozilla.org/en-US/docs/Games/Techniques/Tilemaps/screen_shot_2015-10-06_at_15.56.05.png";
+    class PinkGhostMovement {
 
-    @Test
-    void checkMovement() {
+        public HashMap<Direction, Tile> pseudoSurr;
+        public Ghost pinkie;
+        public PinkGhostMovement() {
+            pinkie = new Ghost.Pink(0,0,10, Color.PINK);
+            pseudoSurr = new HashMap<Direction, Tile>();
+        }
 
+        @Test
+        void checkOnlyLeftMovement() {
+
+        }
     }
+
+
 }
