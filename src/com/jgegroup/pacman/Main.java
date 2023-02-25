7677f9bab1ceddace4f30986c319ccb36aaf06fd
leeant22 <leeant22@uw.edu>
2023-02-25 14:38:45 -0800

    The Button does not show up

--- src/com/jgegroup/pacman/Main.java
+++ src/com/jgegroup/pacman/Main.java
@@ -1,12 +1,30 @@
 package com.jgegroup.pacman;
+import javafx.stage.Stage;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     //Code starts here
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
+        JButton button = new JButton("Click to Start");
+        button.setBounds(20,40,5,5);
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                Stage stage = new Stage();
+                GameCycle gamecycle = new GameCycle();
+                try {
+                    gamecycle.start(stage);
+                }
+                catch(Exception error) {
 
+                }
+            }
+        });
+        frame.add(button);
         //Visibility
         frame.setTitle("Pac Chilling Game");
         frame.setVisible(true);
