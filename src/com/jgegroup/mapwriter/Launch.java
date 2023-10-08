8e6d7d4338bb7a01c434437e705de5d8af732f12
Gabriel Sison <108769772+Gabriel-Sison@users.noreply.github.com>
2023-10-07 17:28:08 -0700

    Update Launch.java

--- src/com/jgegroup/mapwriter/Launch.java
+++ src/com/jgegroup/mapwriter/Launch.java
@@ -2,8 +2,10 @@ package com.jgegroup.mapwriter;
 
 import com.jgegroup.mapwriter.config.Config;
 import com.jgegroup.mapwriter.config.ConfigBuilder;
-import com.jgegroup.pacman.GameScene;
+import com.jgegroup.pacman.GameLaunch;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Side;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -11,6 +13,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.Slider;
 
 import java.io.*;
 
@@ -50,23 +53,86 @@ public class Launch extends Application {
         Pane root = new Pane();
         Label label = new Label ("Game Settings");
         label.relocate(150, 10);
-        TextField textField = new TextField();
-        textField.relocate(100, 30);
-        Button button = new Button ("Button");
-        button.relocate(275,30);
-        root.getChildren().addAll(label, textField, button);
+
+        // Lives for PacMan
+        Slider pacManLivesSlider = new Slider(1, 10, 3);
+        pacManLivesSlider.setPrefWidth(250);
+        pacManLivesSlider.relocate(125, 100);
+        pacManLivesSlider.setBlockIncrement(1);
+        pacManLivesSlider.setMajorTickUnit(1);
+        pacManLivesSlider.setMinorTickCount(0);
+        pacManLivesSlider.setShowTickMarks(true);
+        pacManLivesSlider.setShowTickLabels(true);
+        Label pacManLives = new Label ("Pacman Lives");
+        pacManLives.relocate(25, 100);
+
+        // Speed for Ghost
+        Slider ghostLivesSlider = new Slider(1, 5, 3);
+        ghostLivesSlider.setPrefWidth(250);
+        ghostLivesSlider.relocate(125, 150);
+        ghostLivesSlider.setBlockIncrement(1);
+        ghostLivesSlider.setMajorTickUnit(1);
+        ghostLivesSlider.setMinorTickCount(0);
+        ghostLivesSlider.setShowTickMarks(true);
+        ghostLivesSlider.setShowTickLabels(true);
+        Label ghostSpeed = new Label ("Ghost Speed");
+        ghostSpeed.relocate(25, 150);
+
+        // PacMan Speed default
+        Slider pacManSpeedSlider = new Slider(1, 5, 3);
+        pacManSpeedSlider.setPrefWidth(250);
+        pacManSpeedSlider.relocate(125, 200);
+        pacManSpeedSlider.setBlockIncrement(1);
+        pacManSpeedSlider.setMajorTickUnit(1);
+        pacManSpeedSlider.setMinorTickCount(0);
+        pacManSpeedSlider.setShowTickMarks(true);
+        pacManSpeedSlider.setShowTickLabels(true);
+        Label pacManSpeed = new Label ("Pacman Speed");
+        pacManSpeed.relocate(25, 200);
+
+        Button button = new Button ("Game Launch");
+        button.relocate(300,300);
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                System.out.println("button pressed");
+                int pacmanLives = (int) (pacManLivesSlider.getValue() + 0.5);
+                int ghostSpeed = (int) (ghostLivesSlider.getValue() + 0.5) * 2;
+                int pacmanSpeed = (int) (pacManSpeedSlider.getValue() + 0.5) * 2;
+                System.out.println("Our lives for pacman is " + pacmanLives);
+                System.out.println("Our speed for ghost  is " + ghostSpeed);
+                System.out.println("Our speed for pacman is " + pacmanSpeed);
+                // How do I launch the game from here?
+
+                // We're experimenting with this :)
+                try {
+                    String folderName = "gameSettings";
+                    String fileName = "GUIsettings.txt";
+                    File folder = new File(folderName);
+                    FileWriter file = new FileWriter(fileName);
+                    file.write("this worked");
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        };
+
+        // Create print stream
+
+
+        // when button is pressed
+        button.setOnAction(event);
+        root.getChildren().addAll(label, button, pacManLivesSlider, ghostLivesSlider, pacManSpeedSlider,
+                ghostSpeed, pacManSpeed, pacManLives);
         return root;
     }
 
+    public GameLaunch gameLaunch;
     public Pane MapWriterContent() {
         Pane root = new Pane();
         Label label = new Label ("Map Writer");
         label.relocate(150, 10);
-        TextField textField = new TextField();
-        textField.relocate(150, 30);
-        Button button = new Button ("Button");
-        button.relocate(275,30);
-        root.getChildren().addAll(label, textField, button);
+
+        root.getChildren().addAll(label);
 
         Map map1 = new Map("map2", 10, 10);
         String content = readMap(map1.getPath());
